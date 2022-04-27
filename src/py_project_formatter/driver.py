df17a09d7ec9c56553d84d70e842aebed7a5aa92
elisman14 <elisman14@mail.ru>
2022-04-27 03:34:52 +0300

    Fixing bug with file writing

--- src/py_project_formatter/driver.py
+++ src/py_project_formatter/driver.py
@@ -6,9 +6,9 @@ from py_project_formatter.imports_formatter import sort_imports
 def do_task():
     paths: list[str] = find_files()
     for path in paths:
-        with open(path, 'r+') as opened_file:
-            sorted_file: str = sort_imports(opened_file.read())
-            opened_file.truncate(0)
-            opened_file.write(sorted_file)
+        with open(path, "r") as f:
+            sorted_file: str = sort_imports(f.read())
+        with open(path, "w") as f:
+            f.write(sorted_file)
 
 
